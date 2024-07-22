81c7056a289f02a5a2d9590a2f504ab992119ea3
FelipOliveira <sem.ideias.na.hora@gmail.com>
2024-07-22 16:33:17 -0300

    first commit

---
+++ src/main/java/com/br/foliveira/test_pacto/TestPactoApplication.java
@@ -0,0 +1,13 @@
+package com.br.foliveira.test_pacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPactoApplication.class, args);
+	}
+
+}
